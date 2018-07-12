82dc7627df305c1840862c51016d8cecaef2b623
timflannagan1 <timflannagan@gmail.com>
2018-07-12 15:16:00 -0400

    Add more test functions

--- test.py
+++ test.py
@@ -6,74 +6,132 @@ import subprocess
 # Verify LVM Setup using lvs, vgs, pvs 
 
 def setup_git_repo():
-	git_ret = subprocess.call('git clone https://github.com/dwlehman/linux-storage-role', shell=True)
+    git_ret = subprocess.call('git clone https://github.com/dwlehman/linux-storage-role', shell=True)
 
-	if git_ret:
-		return True
+    if git_ret:
+        return True
 
-	return False
+    return False
 
 def get_vgs():
-	'''Return a list of used volume group names'''
-	result = []
+    '''Return a list of used volume group names'''
+    result = []
 
-	try:
-		vgs_cmd = subprocess.check_output("sudo vgs --no-headings | awk '{ print $1 }'", shell=True)
-		vgs = vgs_cmd.split()
+    try:
+        vgs_cmd = subprocess.check_output("sudo vgs --no-headings | awk '{ print $1 }'", shell=True)
+        vgs = vgs_cmd.split()
 
-		for (counter, vg) in enumerate(vgs):
-			# For debugging purposes atm
-			print('VG_%d: %s' % (counter, vg))
-			result.append(vg)
+        for (counter, vg) in enumerate(vgs):
+            result.append(vg)
                 
-	except subprocess.CalledProcessError as e:
-	    print('Exception from vgs subprocess command: ', e.output)
-	    
-	return result
+    except subprocess.CalledProcessError as e:
+        print('Exception from vgs subprocess command: ', e.output)
+        
+    return result
 
 def get_num_tests(test_file_path):
-	'''Parse test_file_path parameter and return number of tests used in file'''
-	with open(test_file_path, 'r') as test_file:
-		line = test_file.read()
-                num_tests = line.count('include_role')
+    '''Parse test_file_path parameter and return number of tests used in file'''
+    with open(test_file_path, 'r') as test_file:
+        lines = test_file.read()
+        num_tests = lines.count('include_role')
+        
+        # Do parsing/populating here
+    return num_tests
 
-	return num_tests
+def verify_vgs(device_type, actual_vgs, expected_vg):
+    if device_type == 'disk' and expected_vg == '':
+        return True
+    elif expected_vg in actual_vgs:
+        return True
+
+    return False 
+
+def verify_mount(device_name, expected_mount):
+    lsblk_cmd = ("lsblk | grep %s | awk '{ print $1, $7 }'" % device_name)
+    lsblk_buf = subprocess.check_output(lsblk_cmd, shell=True)
+    lsblk_buf = lsblk_buf[6:].replace('\n', '')
+    
+    lsblk_buf = lsblk_buf.split()
+
+    # Not entirely sure what is suppose to come first in the 'in' keyword
+    if (not lsblk_buf[0] in device_name) or (not lsblk_buf[1] in expected_mount):
+        return False 
+
+    return True
+
+# Might need to split this up into two separate functions
+def verify_fs_type(device_name, expected_fs_type):
+    blkid_cmd = ("blkid | grep %s | awk '{ print $3 }'" % device_name)
+    blkid_buf = subprocess.check_output(blkid_cmd, shell=True)
+    blkid_buf = blkid_buf[5:].replace('\n', '').replace('"', '')
+
+    cat_cmd = ("cat /etc/fstab | grep %s | awk '{ print $1, $3 }'" % device_name)
+    cat_buf = subprocess.check_output(cat_cmd, shell=True)
+    cat_buf = cat_buf.split()
+
+    if (not device_name in cat_buf[0]) or (not expected_fs_type in cat_buf[1]):
+        print('***Check failed with differing information in /etc/fstab file***')
+        return False
+
+    if not blkid_buf in expected_fs_type:
+        print('***Check failed with differing file system type in blkid***')
+        return False
+
+    return True
 
 def run_tests(test_file_path):
-	'''Driver for test framework'''
-	num_successes = 0
-	num_tests = get_num_tests(test_file_path)
+    '''Driver for test framework'''
+    num_successes = 0
+    num_tests = get_num_tests(test_file_path)
 
-	# Not used right now
-	ansible_run_cmd = 'ansible-playbook -K tests/test.yml'
+    # Not used right now
+    ansible_run_cmd = 'ansible-playbook -K tests/test.yml'
 
-	# Not used right now
-	vgs_list = get_vgs()
+    # Not used right now
+    vgs_list = get_vgs()
+    test_list = []
 
-	# Call the ansible_run_cmd and verify results from expectations
-	# If test is successful, increment number of successes, else pass
-	# Improve by having better debugging; what test failed and why
-	# Could do this with a dictionary 
+    for test in range(num_tests):
+        test_list.append({'device_type': 'lvm', 'device_name': None, 'disks': None, 'lvm_vg': None, 'mount_point': None, 'size': '100%', 'state': 'present', 'fs_type': 'xfs' })
 
-	return num_successes, num_tests
+    for (counter, test) in enumerate(test_list):
+        fail = False
+
+        if not verify_vgs('disk', vgs_list, ''):
+            fail = True 
+            print('Test %d failed with volume groups' % counter)
+            
+        if not verify_mount('foo-test1', '/opt/test1/'):
+            fail = True
+            print('Test %d failed with mount points' % counter)
+
+        if not verify_fs_type('foo-test1', 'ext'):
+            fail = True
+            print('Test %d failed with different file system types' % counter)
+
+        if not fail:
+            print('Test %d passed all tests' % counter)
+            num_successes += 1
+
+    # Call the ansible_run_cmd and verify results from expectations
+    # If test is successful, increment number of successes, else pass
+    # Improve by having better debugging; what test failed and why
+    # Could do this with a dictionary 
+
+    return num_successes, num_tests
 
 def main():
-<<<<<<< HEAD
-=======
-	# This is pretty messy
->>>>>>> 4bea77cc2e7e9efce18eb9228ab47e44a48ea4e4
     locate_cmd = "locate -w linux-storage-role | awk 'NR==1 { print $1 }'"
     locate_buf = subprocess.check_output(locate_cmd, shell=True)
     locate_buf = locate_buf.replace('\n', '')
 
     if not len(locate_buf):
-            setup_git_repo()
-            file_path = '~/linux-storage-role/tests/test.yml'
+        setup_git_repo()
+        file_path = '~/linux-storage-role/tests/test.yml'
 
     file_path = locate_buf + '/tests/test.yml'
-	num_success, num_tests = run_tests(file_path)
-
-	print('> Testing results: %d/%d total' % (num_success, num_tests))
+    num_success, num_tests = run_tests(file_path)
+    print('> Testing results: %d/%d total' % (num_success, num_tests))
 
 if __name__ == '__main__':
-	main()
+    main()
