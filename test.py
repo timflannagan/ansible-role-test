6e460a58e0d54c18111c6b72549f6974da7817fc
timflannagan1 <timflannagan@gmail.com>
2018-07-12 13:16:41 -0400

    Clean up code and add initial docstrings

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import subprocess
 # Verify LVM Setup using lvs, vgs, pvs 
 
 def setup_git_repo():
-	git_ret = subprocess.call('git clone -b task-flow https://github.com/dwlehman/linux-storage-role', shell=True)
+	git_ret = subprocess.call('git clone https://github.com/dwlehman/linux-storage-role', shell=True)
 
 	if git_ret:
 		return True
@@ -14,18 +14,25 @@ def setup_git_repo():
 	return False
 
 def get_vgs():
+	'''Return a list of used volume group names'''
+	result = []
+
 	try:
 		vgs_cmd = subprocess.check_output("sudo vgs --no-headings | awk '{ print $1 }'", shell=True)
 		vgs = vgs_cmd.split()
 
 		for (counter, vg) in enumerate(vgs):
+			# For debugging purposes atm
 			print('VG_%d: %s' % (counter, vg))
+			result.append(vg)
                 
 	except subprocess.CalledProcessError as e:
 	    print('Exception from vgs subprocess command: ', e.output)
-	    return 
+	    
+	return result
 
 def get_num_tests(test_file_path):
+	'''Parse test_file_path parameter and return number of tests used in file'''
 	with open(test_file_path, 'r') as test_file:
 		line = test_file.read()
                 num_tests = line.count('include_role')
@@ -33,25 +40,34 @@ def get_num_tests(test_file_path):
 	return num_tests
 
 def run_tests(test_file_path):
+	'''Driver for test framework'''
 	num_successes = 0
 	num_tests = get_num_tests(test_file_path)
 
-	run_cmd = 'ansible-playbook -K tests/test.yml'
+	# Not used right now
+	ansible_run_cmd = 'ansible-playbook -K tests/test.yml'
 
+	# Not used right now
 	vgs_list = get_vgs()
+
+	# Call the ansible_run_cmd and verify results from expectations
+	# If test is successful, increment number of successes, else pass
+	# Improve by having better debugging; what test failed and why
+	# Could do this with a dictionary 
 
 	return num_successes, num_tests
 
 def main():
-        locate_cmd = "locate -w linux-storage-role | awk 'NR==1 { print $1 }'"
-        locate_buf = subprocess.check_output(locate_cmd, shell=True)
-        locate_buf = locate_buf.replace('\n', '')
+	# This is pretty messy
+    locate_cmd = "locate -w linux-storage-role | awk 'NR==1 { print $1 }'"
+    locate_buf = subprocess.check_output(locate_cmd, shell=True)
+    locate_buf = locate_buf.replace('\n', '')
 
-        if not len(locate_buf):
-                setup_git_repo()
-                file_path = '~/linux-storage-role/tests/test.yml'
+    if not len(locate_buf):
+            setup_git_repo()
+            file_path = '~/linux-storage-role/tests/test.yml'
 
-        file_path = locate_buf + '/tests/test.yml'
+    file_path = locate_buf + '/tests/test.yml'
 	num_success, num_tests = run_tests(file_path)
 
 	print('> Testing results: %d/%d total' % (num_success, num_tests))
