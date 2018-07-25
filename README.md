e649ab4848129849819a10b7bd00a18404de7a23
timflannagan1 <timflannagan@gmail.com>
2018-07-25 16:06:23 -0400

    Clean up code and add docstrings

--- README.md
+++ README.md
@@ -5,22 +5,8 @@ Runs an ansible playbook and verifies those results against available system inf
 
 To-Do:
 ======
-1. Refactor the names of the verify_fs and verify_mount:
-	- These functions verify the same things, but use different methods.
-2. Implement the verify_size_percentage function.
-    - This should be the last big thing to implement.
-3. Refactor the return value on most of these functions.
-4. Clean up redundant functions.
-5. Don't need to worry about %FREE except in the default 100%FREE specification.
-6. Test the %VG function with MB data instead of GB.
-	- This needs to be refactored some point soon.
-7. Modify the parameters being passes to most functions.
-	- Some of the helper functions are being passed data they don't need to touch.
-8. Modify verify_vg_size_percentage to return failed (flag), used_space, free_space, total_space.
-	- The helper function for verify_pv_size_percentage could use this too.
-	- Assuming that this isn't the greatest idea.
-9. verify_vg_size_percentage is very messy atm.
-10. Re-implement verify_disk_mount
+1. Implement verify_present_status
+2. 
 
 Current Problems:
 =====
@@ -28,3 +14,4 @@ Current Problems:
 	- This isn't the case when there's isn't enough space in the PV to accommodate the specified percentage. It will shrink to the remaining space. 
 2. %VG isn't idempotent, which could make verification difficult.
 	- Usually the ansible-playbook will error though.
+3. Think about what to check when device type is 'lvm' in is_invalid function
--- library/role_test.py
+++ library/role_test.py
@@ -3,17 +3,15 @@
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils import facts
 import subprocess
-import math
 
 def verify_absent_fstab_info(role_dict, failed_tests):
-    was_failed = False
+    '''Check if the info specified in the role_dict is absent in the /etc/fstab file.'''
     found_line = False
     implemented = True
-    line_buf = []
 
     if not implemented:
         failed_tests.append('Check failed as verify_absent_fstab_info has not been implemented.')
-        was_failed = True
+        return True
 
     if role_dict['device_type'] is not None and role_dict['device_type'] in 'disk':
         expected_name = '/dev/' + role_dict['disks'][0]
@@ -24,72 +22,42 @@ def verify_absent_fstab_info(role_dict, failed_tests):
         for line in file.readlines():
             if expected_name in line:
                 found_line = True
-                line_buf = line.replace('\n', '').split(' ')
                 break
 
     if found_line:
-        if role_dict['device_type'] is not None and role_dict['device_type'] in 'disk':
-            failed_tests.append('Debug check: {}'.format(line_buf))
-            was_failed = True
-        else:
-            failed_tests.append('Check failed as device type is lvm but there is still a listing for the device in /etc/fstab file.')
-            was_failed = True
-
-    return was_failed
+        failed_tests.append('Check failed as there is still info about device in /etc/fstab file.')
+        return True
 
 def absent_disk_helper(role_dict, failed_tests):
     '''Helper function for verifying that specified disk is unmounted, but present in lsblk output.'''
-    was_failed = False
-    implemented = True
-
-    if not implemented:
-        failed_tests.append('Check failed as absent_disk_helper has not been implemented.')
-        was_failed = True
-
     expected_name = '/dev/' + role_dict['disks'][0]
 
-    lsblk_cmd = "lsblk %s -o name,mountpoint --noheadings" % expected_name
+    lsblk_cmd = "lsblk %s -o name,fstype,mountpoint --noheadings" % expected_name
     lsblk_buf = subprocess.check_output(lsblk_cmd, shell=True).split()
 
     if len(lsblk_buf) is not 1:
-        failed_tests.append('Debug: length of lsblk buffer was not required length in absent_disk_helper.')
-        was_failed = True
+        failed_tests.append('Debug: lsblk command failed to return required number of fields (most likely incorrect syntax in .yml file).')
+        return True
 
     if lsblk_buf[0] not in role_dict['disks'][0]:
         failed_tests.append('Check failed as the name of the disk specified in the .yml file does not match the lsblk output.')
-        was_failed = True
-
-    return was_failed
-
-def absent_lvm_helper(role_dict, failed_tests):
-    '''Helper function for verifying that specified lvm device is not present in lsblk command output.'''
-    was_failed = False
-    implemented = True
-
-    if not implemented:
-        failed_tests.append('Check failed as absent_lvm_helper has not been implemented.')
         return True
 
+def absent_lvm_helper(role_dict, failed_tests):
+    '''Helper function for verifying that specified lvm device is not present in lvs command output.'''
     try:
         lvs_cmd = "sudo lvs %s -o name --no-headings" % role_dict['lvm_vg']
         lvs_buf = subprocess.check_output(lvs_cmd, shell=True).replace('\n', '').split()
     except subprocess.CalledProcessError as e:
-        # This should happen if there's no LVs in the specified VG anymore
-        return True
+        return False
 
     if role_dict['device_name'] in lvs_buf:
         failed_tests.append('Check failed as device name %s was found in lvs output when run in absent state.' % role_dict['device_name'])
-        was_failed = True
-
-    return was_failed
+        return True
 
 def verify_absent_mount_info(role_dict, failed_tests):
+    '''Run helper functions and return the was_failed flag.'''
     was_failed = False
-    implemented = True
-
-    if not implemented:
-        failed_tests.append('Check failed as verify_absent_mount_info has not been implemented.')
-        return True
 
     if role_dict['device_type'] is not None and role_dict['device_type'] in 'disk':
         was_failed = absent_disk_helper(role_dict, failed_tests)
@@ -99,29 +67,47 @@ def verify_absent_mount_info(role_dict, failed_tests):
     return was_failed
 
 def verify_absent_state(role_dict, test_run_results):
+    '''Driver function that handles verification when state is set as absent.'''
     was_failed = False 
-    implemented = True
 
-    if not implemented:
-        test_run_results['tests_failed'].append('Check failed as verify_absent_state is not yet implemented.')
-        return True
-
-    if not verify_absent_fstab_info(role_dict, test_run_results['tests_failed']):
+    if verify_absent_fstab_info(role_dict, test_run_results['tests_failed']):
         was_failed = True
     else:
         test_run_results['num_successes'] += 1
 
-    if not verify_absent_mount_info(role_dict, test_run_results['tests_failed']):
+    if verify_absent_mount_info(role_dict, test_run_results['tests_failed']):
         was_failed = True
     else:
         test_run_results['num_successes'] += 1
 
-    # verify lvm: lvs; verify that the disk has no mount point or filesystem
     test_run_results['num_tests'] += 2
     
     return was_failed
 
+def verify_present_default_size(role_dict, failed_tests):
+	return True
+
+def verify_present_percentage_size(role_dict, failed_tests):
+	return True
+
+def verify_present_specified_size(role_dict, failed_tests):
+	return True
+
+def verify_present_size(role_dict, failed_tests):
+	'''Check the format of the size variable in role_dict, and call the corresponding verification function.'''
+	was_failed = False
+
+	if role_dict['size'] is None or role_dict['size'] in '100%FREE':
+		was_failed = verify_present_default_size(role_dict, failed_tests)
+	else if role_dict['size'] is not None and '%' in role_dict['size']:
+		was_failed = verify_present_percentage_size(role_dict, failed_tests)
+	else:
+		was_failed = verify_present_specified_size(role_dict, failed_tests)
+
+	return was_failed
+
 def verify_present_state(role_dict, test_run_results):
+    '''Driver function that handles verification when state is set as present.'''
     was_failed = False
     implemented = False
 
@@ -129,9 +115,15 @@ def verify_present_state(role_dict, test_run_results):
         test_run_results['tests_failed'].append('Check failed as verify_present_state is not yet implemented.')
         return True
 
+    if verify_present_size(role_dict, test_run_results['tests_failed']):
+    	was_failed = True
+    else:
+    	tests_run_results['num_successes'] += 1
+
     return was_failed
 
 def is_invalid(role_dict, failed_list):
+    '''Check that the .yml file uses the correct fields before running verification functions.'''
     was_failed = False
 
     if role_dict['device_type'] is 'disk':
@@ -181,7 +173,7 @@ def run_module():
 
     # Do all the policing of the .yml file first
     if is_invalid(module.params, results['tests_failed']):
-        module.exit_json(msg={}).format(results['tests_failed'])
+        module.exit_json(msg='Invalid: {}').format(results['tests_failed'])
 
     if module.params['state'] is None or module.params['state'] in 'present':
         test_failed = verify_present_state(module.params, results)
